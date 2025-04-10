c8a7078234bd4536e2d1f3142f699ab90c2510c3
cnwang <cnwang08@gmail.com>
2025-04-10 20:20:38 +0800

    34.在排序数组中查找元素的第一个和最后一个位置

--- Hot100/BinarySearch/FindFirstAndLastPositionOfElementInSortedArray.java
+++ Hot100/BinarySearch/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,5 +1,6 @@
 package LeetCode.Hot100.BinarySearch;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,9 +9,47 @@ import java.util.Scanner;
  */
 public class FindFirstAndLastPositionOfElementInSortedArray {
 
+    public static int[] FindFirstAndLastPositionOfElementInSortedArray(int[] nums,int target){
+
+        int l = BinarySearch(nums,target);
+        if(l==nums.length || nums[l]!=target) {
+            return new int[]{-1, -1};
+        }else{
+            int r = BinarySearch(nums,target+1);
+            return new int[]{l,r-1};
+        }
+    }
+
+    public static int BinarySearch(int[] nums,int target){
+        int len = nums.length;
+        int left = 0;
+        int right = len-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]<target){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        String s = sc.nextLine().replaceAll("[^\\d,-]","");
+        int target = sc.nextInt();
+        String[] split = s.split(",");
+        int[] nums = new int[split.length];
+        for(int i=0;i< nums.length;i++){
+            if(split[i].isEmpty()){
+                nums[i] = Integer.MIN_VALUE;
+            }else {
+                nums[i] = Integer.parseInt(split[i]);
+            }
+        }
+        int[] res = FindFirstAndLastPositionOfElementInSortedArray(nums,target);
+        System.out.println(Arrays.toString(res));
     }
 
 }
