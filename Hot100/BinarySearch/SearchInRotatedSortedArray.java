a3446a8d0694b9536fd95e5435856f78944581b1
cnwang <cnwang08@gmail.com>
2025-04-10 20:53:52 +0800

    33.搜索旋转排序数组

---
+++ Hot100/BinarySearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,50 @@
+package LeetCode.Hot100.BinarySearch;
+
+import java.util.Scanner;
+
+/**
+ * @Author cnwang
+ * @Date created in 20:21 2025/4/10
+ */
+public class SearchInRotatedSortedArray {
+
+    public static int SearchInRotatedSortedArray(int[] nums,int target){
+        int len = nums.length;
+        int left = 0;
+        int right = len-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else{
+                if(nums[mid]>nums[right]){//左边有序
+                    if(nums[mid]>target && target>=nums[left]){
+                        right = mid-1;
+                    }else{
+                        left = mid+1;
+                    }
+                }else{
+                    if(nums[mid]<target && target<=nums[right]){
+                        left = mid+1;
+                    }else{
+                        right = mid-1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().replaceAll("[^\\d,-]","");
+        int target = sc.nextInt();
+        String[] split = s.split(",");
+        int[] nums = new int[split.length];
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = Integer.parseInt(split[i]);
+        }
+        System.out.println(SearchInRotatedSortedArray(nums,target));
+    }
+
+}
