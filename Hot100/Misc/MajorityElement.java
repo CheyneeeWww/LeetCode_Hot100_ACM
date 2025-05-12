a7c5cd0bdba56ee157b8becb25bbddf5ee8ae21f
cnwang <cnwang08@gmail.com>
2025-05-12 19:14:51 +0800

    技巧题1+2

--- Hot100/Misc/MajorityElement.java
+++ Hot100/Misc/MajorityElement.java
@@ -11,17 +11,18 @@ public class MajorityElement {
         int x = 0;
         int votes = 0;
         for(int num:nums){
-            if(votes ==0){
+            if(votes == 0){
                 x = num;
             }
-            votes += num == x? 1:-1;
+            votes += num == x?1:-1;
         }
         return x;
     }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine().replaceAll("[^\\d,-]","");
-        String[] split = s.split(",");
+        String s = sc.nextLine();
+        String[] split = s.split(" ");
         int[] nums = new int[split.length];
         for(int i=0;i<nums.length;i++){
             nums[i] = Integer.parseInt(split[i]);
